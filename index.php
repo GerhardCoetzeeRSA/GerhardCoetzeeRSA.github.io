cdf6070f2d5fc679562e4c7e8b4a5d2fefe7cbac
Gerhard Coetzee <gerhardseepos@gmail.com>
2018-05-14 16:40:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Step1
- $db = mysqli_connect('localhost','Gabba1','Biamwu123!@#','https://c-gerhaylx.aserv.co.za:2083/cpsess8222686962/3rdparty/phpMyAdmin/db_structure.php?db=gerhaylx_GCoetzee_Email_Form&token=22a75f9abbd9371f8f9cc1fc02de91e5')
+ $db = mysqli_connect('localhost','Gabba1','Biamwu123!@#','https://c-gerhaylx.aserv.co.za:2083/cpsess8222686962/3rdparty/phpMyAdmin/sql.php?db=gerhaylx_GCoetzee_Email_Form&table=Portfolio_Address_Form&token=22a75f9abbd9371f8f9cc1fc02de91e5')
  or die('Error connecting to MySQL server.');
 ?>
 
